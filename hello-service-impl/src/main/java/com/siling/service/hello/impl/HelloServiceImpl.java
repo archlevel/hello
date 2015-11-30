a5cd00dedd1d26cc11a6165aab124a9be0e7df7c
zhu.gongping <zhu.gongping@kjt.com>
2015-11-30 12:58:53 +0800

    优化依赖

--- hello-service-impl/src/main/java/com/siling/service/hello/impl/HelloServiceImpl.java
+++ hello-service-impl/src/main/java/com/siling/service/hello/impl/HelloServiceImpl.java
@@ -1,32 +1,15 @@
 package com.siling.service.hello.impl;
 
-import javax.annotation.Resource;
-
 import org.springframework.transaction.annotation.Transactional;
 
 import com.siling.service.hello.IHelloService;
-import com.siling.service.hello.dao.ICodeDAO;
-import com.siling.service.hello.dao.model.Code;
 import com.tower.service.impl.AbsServiceImpl;
 
 public class HelloServiceImpl extends AbsServiceImpl implements IHelloService {
-	@Resource(name="Code")
-	private ICodeDAO<Code> codeDAOImpl;
+	
 	@Override
 	@Transactional
 	public void sayHello() {
-		long start = System.currentTimeMillis();
-		Code code = codeDAOImpl.queryById(15, null);
-		System.out.println((System.currentTimeMillis()-start));
-		int cnt = 0;
-		while(true){
-			cnt++;
-			start = System.currentTimeMillis();
-			code = codeDAOImpl.queryById(15, null);
-			System.out.println((System.currentTimeMillis()-start));
-			if(cnt>=100){
-				break;
-			}
-		}
+		System.out.println("hello");
 	}
 }
