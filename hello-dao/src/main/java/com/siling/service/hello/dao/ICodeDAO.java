0e4e26d62eb09acfd8d1778899f9cea47ee1f3de
zhu.gongping <zhu.gongping@kjt.com>
2015-12-09 21:44:44 +0800

    open spring-service.xml hello service

--- hello-dao/src/main/java/com/siling/service/hello/dao/ICodeDAO.java
+++ hello-dao/src/main/java/com/siling/service/hello/dao/ICodeDAO.java
@@ -1,9 +1,10 @@
 package com.siling.service.hello.dao;
 
-import com.siling.service.hello.dao.model.Code;
 import com.tower.service.dao.IDAO;
 import com.tower.service.dao.IFKDAO;
 import com.tower.service.dao.IIDAO;
+
+import com.siling.service.hello.dao.model.Code;
 
 public interface ICodeDAO<T extends Code> extends IIDAO<T>,IFKDAO<T>,IDAO<T> {
 
--- hello-dao/src/main/java/com/siling/service/hello/dao/ibatis/mapper/CodeMapper.java
+++ hello-dao/src/main/java/com/siling/service/hello/dao/ibatis/mapper/CodeMapper.java
@@ -1,11 +1,9 @@
 package com.siling.service.hello.dao.ibatis.mapper;
 
-import java.util.List;
-import java.util.Map;
-
-import com.siling.service.hello.dao.model.Code;
 import com.tower.service.dao.ibatis.IIMapper;
 
+import com.siling.service.hello.dao.model.Code;
+
 public interface CodeMapper extends IIMapper<Code>{
- public List<Map> aa(Map params);
+
 }
